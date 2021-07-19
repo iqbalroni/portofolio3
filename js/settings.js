50879a16a020b4516bfe37ea9096c8e10e447a4b
IqbalRoni <roniesaputra92@gmail.com>
2021-07-19 10:21:25 +0700

    settings

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+let iconsettings = document.getElementById('set');
+let pagesettings = document.querySelector('.settings');
+
+iconsettings.onclick = function(){
+    pagesettings.classList.add('open');
+}
