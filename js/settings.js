752d1f4b581d2836b0e81f799f6c2172c316eee9
IqbalRoni <roniesaputra92@gmail.com>
2021-07-22 12:44:43 +0700

    audio

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,12 @@
 let iconsettings = document.getElementById('set');
 let pagesettings = document.querySelector('.settings');
 
+let closeSetting = document.getElementById('set-close');
+
 iconsettings.onclick = function(){
     pagesettings.classList.add('open');
+}
+
+closeSetting.onclick = function(){
+    pagesettings.classList.remove('open');
 }
